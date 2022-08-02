0e2f1f6e4dddc9470c4e92c9999eaf09e63050e5
ericsweeten <eric.sweeten@gmail.com>
2022-08-02 11:48:09 -0700

    Initial social media web API

---
+++ SocialMediaWebAPI/SocialMediaWebAPI/Models/EF/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMediaWebAPI.Models.EF
+{
+    public partial class Post
+    {
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string? Message { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? Image { get; set; }
+
+        public virtual User? User { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ SocialMediaWebAPI/SocialMediaWebAPI/Models/EF/UserFriend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMediaWebAPI.Models.EF
+{
+    public partial class UserFriend
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int? FriendId { get; set; }
+        public string? FriendStatus { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
