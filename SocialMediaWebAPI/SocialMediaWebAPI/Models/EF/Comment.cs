927d2222c0df1272ddeba80319fdcf8b300c2c68
ericsweeten <eric.sweeten@gmail.com>
2022-08-03 12:56:15 -0700

    Latest API

---
+++ SocialMediaWebAPI/SocialMediaWebAPI/Models/EF/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMediaWebAPI.Models.EF
+{
+    public partial class Comment
+    {
+        public int CommentId { get; set; }
+        public string? Comment1 { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? PostId { get; set; }
+        public string? CommentImage { get; set; }
+
+        public virtual Post? Post { get; set; }
+    }
+}
