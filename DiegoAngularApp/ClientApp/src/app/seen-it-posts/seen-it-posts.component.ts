d6827e3b9e510628ce73eab1c9a2ab1f78af1f49
Diegoortiz55 <diego.ortiz3412@gmail.com>
2022-08-04 14:50:40 -0400

    Diego's Angular App

---
+++ DiegoAngularApp/ClientApp/src/app/seen-it-posts/seen-it-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seen-it-posts',
+  templateUrl: './seen-it-posts.component.html',
+  styleUrls: ['./seen-it-posts.component.css']
+})
+export class SeenItPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
