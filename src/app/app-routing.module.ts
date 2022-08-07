3dd3b13c438b889e545568603e1af242905c2ac2
HamezGuy <jamesgui111@gmail.com>
2022-08-06 21:08:14 -0500

    please work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { SeenCommentsPageComponent } from './seen-comments-page/seen-comments-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
+
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'homePage', component: HomePageComponent},
+  {path: 'aboutPage', component: AboutPageComponent},
+  {path: 'seenCommentsPage', component: SeenCommentsPageComponent},
+  {path: 'loginPage', component: LoginPageComponent},
+  {path: 'signUpPage', component: SignUpPageComponent},
+  {path: 'userProfilePage', component: UserProfilePageComponent},
+  {path: 'dashboardPage', component: DashboardPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { SeenCommentsPageComponent } from './seen-comments-page/seen-comments-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    SeenCommentsPageComponent,
+    LoginPageComponent,
+    SignUpPageComponent,
+    UserProfilePageComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
